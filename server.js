1a0e7dbf064da44a5941ec1c419510e889f33ac9
abbyt-16 <114946504+abbyt-16@users.noreply.github.com>
2024-04-24 15:57:34 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,46 +1,46 @@
-const readline = require('readline');
+const express = require('express');
 const { MongoClient } = require('mongodb');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.use(express.urlencoded({ extended: true }));
 
 const mongoURI = 'mongodb+srv://abigailtonkinson:C9cP4SjtnYcwTgaS@stock.qiiykcv.mongodb.net/?retryWrites=true&w=majority&appName=Stock';
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
 MongoClient.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
     if (err) {
         console.error('Failed to connect to the database');
         process.exit(1);
     }
-
     const db = client.db('Stock');
-    rl.question('Enter a company name or stock ticker: ', async (searchTerm) => {
-        try {
-            const collection = db.collection('PublicCompanies');
 
-            let query = {};
-            const searchType = searchTerm.startsWith('^') ? 'ticker' : 'company';
-            if (searchType === 'ticker') {
-                query = { stockTicker: searchTerm.substring(1) };
-            } else {
-                query = { companyName: searchTerm };
-            }
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'index.html'));
+    });
 
-            const cursor = collection.find(query);
-            const results = await cursor.toArray();
-
-            if (results.length === 0) {
-                console.log('No matching companies found');
-            } else {
-                results.forEach(result => {
-                    console.log(`Company Name: ${result.companyName}, Ticker Symbol: ${result.stockTicker}, Stock Price: ${result.stockPrice}`);
-                });
-            }
-        } catch (err) {
-            console.error('Error finding documents:', err);
-        } finally {
-            client.close();
-            rl.close();
+    app.post('/search', async (req, res) => {
+        const searchTerm = req.body.searchTerm;
+        let query = {};
+        const searchType = searchTerm.startsWith('^') ? 'ticker' : 'company';
+        if (searchType === 'ticker') {
+            query = { stockTicker: searchTerm.substring(1) };
+        } else {
+            query = { companyName: searchTerm };
         }
+        
+        const collection = db.collection('PublicCompanies');
+        const cursor = collection.find(query);
+        const results = await cursor.toArray();
+
+        res.json(results);
     });
 });
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
